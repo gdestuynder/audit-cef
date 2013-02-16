58b603190d83b031727a16728e801381a85ef1eb
Guillaume Destuynder <gdestuynder@mozilla.com>
2013-02-15 22:41:48 -0800

    Cleaning message handling a bit Added AVC=>CEF support for some AppArmor messages

--- audisp/plugins/cef/audisp-cef.c
+++ audisp/plugins/cef/audisp-cef.c
@@ -1,5 +1,5 @@
 /* audisp-cef.c --
- * Copyright (c) 2012 Mozilla Corporation.
+ * Copyright (c) 2013 Mozilla Corporation.
  * Portions Copyright 2008 Red Hat Inc., Durham, North Carolina.
  * All Rights Reserved.
  *
@@ -51,10 +51,24 @@ static volatile int hup = 0;
 static cef_conf_t config;
 static char *hostname = NULL;
 static auparse_state_t *au = NULL;
-static struct passwd pwd;
-static char *buf;
-static size_t bufsize;
 static int machine = -1;
+
+typedef struct	ll {
+	char val[1024];
+	struct ll *next;
+} attr_t;
+
+struct cef_msg_type {
+char	*hdr;
+char	*type;
+char	*app;
+int		version;
+char	*msgname;
+char	*msgdesc;
+int		severity;
+struct	ll *attr;
+time_t	au_time;
+};
 
 static void handle_event(auparse_state_t *au,
 		auparse_cb_event_t cb_event_type, void *user_data);
@@ -106,11 +120,6 @@ int main(int argc, char *argv[])
 	hostname = (char *)malloc(sizeof(uts.nodename));
 	sprintf(hostname, "%s", uts.nodename);
 
-	bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
-	if (bufsize == -1)
-		bufsize = 16384;
-	buf = (char *)malloc(bufsize);
-
 	if (load_config(&config, CONFIG_FILE))
 		return 1;
 
@@ -145,7 +154,6 @@ int main(int argc, char *argv[])
 	auparse_flush_feed(au);
 	auparse_destroy(au);
 	free(hostname);
-	free(buf);
 	syslog(LOG_INFO, "audisp-cef unloaded\n");
 	closelog();
 
@@ -171,44 +179,164 @@ static int goto_record_type(auparse_state_t *au, int type)
 	return -1;
 }
 
+char *unescape(char *in)
+{
+	char *dst = in;
+	char *s = dst;
+	char *src = in;
+	char c;
+
+	while ((c = *src++) != '\0') {
+    	if (c != '"')
+        	*dst++ = c;
+	}
+	*dst = '\0';
+	return s;
+}
+
+attr_t *cef_add_attr(attr_t *list, const char *st, char *val)
+{
+	attr_t *new;
+
+	if (val == NULL)
+			return list;
+	if (strstr(val, "(null)") != NULL)
+			return list;
+
+	new = malloc(sizeof(attr_t));
+	snprintf(new->val, 1023, "%s%s ", st, unescape(val));
+	new->next = list;
+	return new;
+}
+
+char *get_username(int uid)
+{
+	size_t bufsize;
+	char *buf;
+	char *name;
+	struct passwd pwd;
+	struct passwd *result;
+
+	bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
+	if (bufsize == -1)
+			bufsize = 16384;
+	buf = (char *)malloc(bufsize);
+
+	if (uid == -1)
+			return NULL;
+	getpwuid_r(uid, &pwd, buf, bufsize, &result);
+	name = strdup(pwd.pw_name);
+	free(buf);
+	return name;
+}
+
+char *get_proc_name(int pid)
+{
+	char p[1024];
+	char proc[64];
+	FILE *fp;
+	snprintf(p, 512, "/proc/%d/status", pid);
+	fp = fopen(p, "r");
+	if (fp) {
+		fscanf(fp, "Name: %63s", proc);
+		fclose(fp);
+	} else
+		return NULL;
+	return proc;
+}
+
+void syslog_cef_msg(struct cef_msg_type cef_msg)
+{
+	attr_t *head = cef_msg.attr;
+	attr_t *prev;
+	char msg[1500];
+
+	snprintf(msg, 1500, "%s|%s|%s|%u|%s|%s|%u|end=%ld ", cef_msg.hdr, cef_msg.type, cef_msg.app,
+		cef_msg.version, cef_msg.msgname, cef_msg.msgdesc, cef_msg.severity, cef_msg.au_time);
+	while (head) {
+			snprintf(msg+strlen(msg), 1500, "%s", head->val);
+			prev = head;
+			head = head->next;
+			free(prev);
+	}
+	syslog(LOG_INFO, msg);
+}
+
 static void handle_event(auparse_state_t *au,
 		auparse_cb_event_t cb_event_type, void *user_data)
 {
 	int type, rc, num=0;
 	time_t au_time;
-	const char *key = NULL, *ppid = NULL, *pid = NULL, *auid = NULL, *uid = NULL, *gid = NULL, *euid = NULL, *suid = NULL;
-	const char *fsuid = NULL, *egid = NULL, *sgid = NULL, *fsgid = NULL, *tty = NULL, *exe = NULL, *ses = NULL;
+
+	struct cef_msg_type cef_msg = {
+		.hdr		= "CEF:0",
+		.type		= "Unix",
+		.app		= "auditd",
+		.version	= 1,
+		.severity	= 3,
+	};
+
 	const char *cwd = NULL, *argc = NULL, *cmd = NULL;
+	char *sys;
 	const char *syscall = NULL;
-	const char *fname = NULL, *inode = NULL, *dev = NULL, *mode = NULL, *ouid = NULL, *ogid = NULL, *rdev = NULL;
 	char fullcmd[MAX_ARG_LEN+1] = "\0";
 	char fullcmdt[5] = "No\0";
-	char extra[1024] = "\0";
-	char extrat[5] = "No\0";
-   const char *sys;
-	int extralen = 0;
-
-	char msgname[16] = "\0", msgdesc[32] = "\0";
-	char sppid[256], ppid_p[1024];
-	FILE *fp;
+	char extra[512] = "\0";
 
 	char f[8];
 	int len, tmplen;
 	int argcount, i;
-	struct passwd *result;
 	int havecef = 0;
 
 	if (cb_event_type != AUPARSE_CB_EVENT_READY)
 		return;
 
-	pwd.pw_name = NULL;
-
 	while (auparse_goto_record_num(au, num) > 0) {
 		type = auparse_get_type(au);
 		rc = 0;
 		auparse_first_field(au);
-		au_time = auparse_get_time(au);
+		cef_msg.au_time = auparse_get_time(au);
 		switch (type) {
+		   	case AUDIT_AVC:
+				argc = auparse_find_field(au, "apparmor");
+				if (!argc)
+					return;
+
+				havecef = 1;
+				cef_msg.msgname = "AVC_APPARMOR";
+
+				cef_msg.attr = cef_add_attr(cef_msg.attr, "cs1Label=Result cs1=", auparse_get_field_str(au));
+				goto_record_type(au, type);
+
+				cef_msg.msgdesc = unescape(auparse_find_field(au, "info"));
+				goto_record_type(au, type);
+
+				cef_msg.attr = cef_add_attr(cef_msg.attr, "cs2Label=Operation cs2=", auparse_find_field(au, "operation"));
+				goto_record_type(au, type);
+
+				cef_msg.attr = cef_add_attr(cef_msg.attr, "cs3Label=Profile cs3=", auparse_find_field(au, "profile"));
+				goto_record_type(au, type);
+
+				cef_msg.attr = cef_add_attr(cef_msg.attr, "cs4Label=Command cs4=", auparse_find_field(au, "comm"));
+				goto_record_type(au, type);
+
+				if (auparse_find_field(au, "parent"))
+					cef_msg.attr = cef_add_attr(cef_msg.attr, "sproc=", get_proc_name(auparse_get_field_int(au)));
+				goto_record_type(au, type);
+
+				if (auparse_find_field(au, "pid"))
+					cef_msg.attr = cef_add_attr(cef_msg.attr, "dproc=", get_proc_name(auparse_get_field_int(au)));
+				goto_record_type(au, type);
+
+				snprintf(extra+strlen(extra), 16, "error\\=%s ", auparse_find_field(au, "error"));
+				goto_record_type(au, type);
+				snprintf(extra+strlen(extra), 16, "name\\=%s ", auparse_find_field(au, "name"));
+				goto_record_type(au, type);
+				snprintf(extra+strlen(extra), 16, "srcname\\=%s ", auparse_find_field(au, "srcname"));
+				goto_record_type(au, type);
+				snprintf(extra+strlen(extra), 16, "flags\\=%s ", auparse_find_field(au, "flags"));
+				goto_record_type(au, type);
+				break;
 			case AUDIT_EXECVE:
 				argc = auparse_find_field(au, "argc");
 				if (argc)
@@ -225,154 +353,126 @@ static void handle_event(auparse_state_t *au,
 					cmd = auparse_interpret_field(au);
 					if (!cmd)
 						continue;
-					if (MAX_ARG_LEN-strlen(fullcmd) > strlen(cmd))
+					if (MAX_ARG_LEN-strlen(fullcmd) > strlen(cmd)) {
 						if (len == 0)
 							len += sprintf(fullcmd+len, "%s", cmd);
 						else
 							len += sprintf(fullcmd+len, " %s", cmd);
-					else
-						strncpy(fullcmdt, "Yes\0", 4);
+					} else
+							strncpy(fullcmdt, "Yes\0", 4);
 				}
+				cef_msg.attr = cef_add_attr(cef_msg.attr, "cs2Label=Truncated cs2=", fullcmdt);
+				cef_msg.attr = cef_add_attr(cef_msg.attr, "cs1Label=Command cs1=", fullcmd);
 				break;
 			case AUDIT_CWD:
 				cwd = auparse_find_field(au, "cwd");
-				if (cwd)
+				if (cwd) {
 					auparse_interpret_field(au);
-				if (extralen == 0)
-					extralen += snprintf(extra+extralen, 1024, "cwd\\=%s", cwd);
-				else
-					extralen += snprintf(extra+extralen, 1024, " cwd\\=%s", cwd);
+					snprintf(extra+strlen(extra), 128, "cwd\\=%s ", cwd);
+				}
 				break;
 			case AUDIT_PATH:
-				fname = auparse_find_field(au, "name");
+				cef_msg.attr = cef_add_attr(cef_msg.attr, "fname=", auparse_find_field(au, "name"));
 				goto_record_type(au, type);
-				inode = auparse_find_field(au, "inode");
+
+				snprintf(extra+strlen(extra), 16, "inode\\=%s ", auparse_find_field(au, "inode"));
 				goto_record_type(au, type);
-				inode = auparse_find_field(au, "dev");
+				snprintf(extra+strlen(extra), 16, "dev\\=%s ", auparse_find_field(au, "dev"));
 				goto_record_type(au, type);
-				inode = auparse_find_field(au, "mode");
+				snprintf(extra+strlen(extra), 16, "mode\\=%s ", auparse_find_field(au, "mode"));
 				goto_record_type(au, type);
-				inode = auparse_find_field(au, "ouid");
+				snprintf(extra+strlen(extra), 16, "ouid\\=%s ", auparse_find_field(au, "ouid"));
 				goto_record_type(au, type);
-				inode = auparse_find_field(au, "ogid");
+				snprintf(extra+strlen(extra), 16, "ogid\\=%s ", auparse_find_field(au, "ogid"));
 				goto_record_type(au, type);
-				inode = auparse_find_field(au, "rdev");
+				snprintf(extra+strlen(extra), 16, "rdev\\=%s ", auparse_find_field(au, "rdev"));
 				goto_record_type(au, type);
-				if (extralen == 0) {
-					extralen += snprintf(extra+extralen, 1024, "inode\\=%s dev\\=%s mode\\=%s ouid\\=%s ogid\\=%s rdev\\=%s",
-						inode, dev, mode, ouid, ogid, rdev);
-				} else {
-					extralen += snprintf(extra+extralen, 1024, " inode\\=%s dev\\=%s mode\\=%s ouid\\=%s ogid\\=%s rdev\\=%s",
-						inode, dev, mode, ouid, ogid, rdev);
-				}
 				break;
 			case AUDIT_SYSCALL:
 				syscall = auparse_find_field(au, "syscall");
 				if (!syscall)
 					return;
 				i = auparse_get_field_int(au);
-               sys = audit_syscall_to_name(i, machine);
-               if (!sys) {
-                   syslog(LOG_INFO, "Unknown system call %u", i);
-                   return;
-               }
+				sys = audit_syscall_to_name(i, machine);
+				if (!sys) {
+					syslog(LOG_INFO, "Unknown system call %u", i);
+					return;
+				}
 
-               if (!strncmp(sys, "write", 5) || !strncmp(sys, "open", 4) || \
-                   !strncmp(sys, "unlink", 6)) {
+				if (!strncmp(sys, "write", 5) || !strncmp(sys, "open", 4) || !strncmp(sys, "unlink", 6)) {
 					havecef = i;
-					strncpy(msgname, "WRITE\0", 6);
-					strncpy(msgdesc, "Write or append to file\0", 24);
-               } else if (!strncmp(sys, "setxattr", 8)) {
+					cef_msg.msgname = "WRITE";
+					cef_msg.msgdesc = "Write or append to file";
+				} else if (!strncmp(sys, "setxattr", 8)) {
 					havecef = i;
-					strncpy(msgname, "ATTR\0", 5);
-					strncpy(msgdesc, "Change file attributes\0", 23);
+					cef_msg.msgname = "ATTR";
+					cef_msg.msgdesc = "Change file attributes";
 				} else if (!strncmp(sys, "chmod", 5)) {
 					havecef = i;
-					strncpy(msgname, "CHMOD\0", 6);
-					strncpy(msgdesc, "CHMOD failed\0", 13);
+					cef_msg.msgname = "CHMOD";
+					cef_msg.msgdesc = "Change file mode";
 				} else if (!strncmp(sys, "chown", 5)) {
 					havecef = i;
-					strncpy(msgname, "CHOWN\0", 7);
-					strncpy(msgdesc, "CHOWN failed\0", 13);
+					cef_msg.msgname = "CHOWN";
+					cef_msg.msgdesc = "Change file owner";
 				} else if (!strncmp(sys, "ptrace",  6)) {
 					havecef = i;
-					strncpy(msgname, "PTRACE\0", 8);
-					strncpy(msgdesc, "PTRACE called\0", 14);
+					cef_msg.msgname = "PTRACE";
+					cef_msg.msgdesc = "Process tracing";
 				} else if (!strncmp(sys, "execve", 6)) {
-                   havecef = i;
-	    			strncpy(msgname, "EXECVE\0", 7);
-		    		strncpy(msgdesc, "Unix Exec\0", 10);
-               } else {
-                   syslog(LOG_INFO, "Unhandled system call %u %s", i, sys);
-               }
-
-				key = auparse_find_field(au, "key");
-				if (key)
-					key = auparse_interpret_field(au);
-				goto_record_type(au, type);
-				ppid = auparse_find_field(au, "ppid");
-				if (ppid) {
-					i = auparse_get_field_int(au);
-					snprintf(ppid_p, 1024, "/proc/%d/status", i);
-					fp = fopen(ppid_p, "r");
-					if (fp) {
-						fscanf(fp, "Name: %255s", sppid);
-						fclose(fp);
-					}
-				}
-
-				goto_record_type(au, type);
-				pid = auparse_find_field(au, "pid");
-				goto_record_type(au, type);
-				auid = auparse_find_field(au, "auid");
-				if (auid) {
-					i = auparse_get_field_int(au);
-					if (i != -1)
-						getpwuid_r(i, &pwd, buf, bufsize, &result);
-				}
-				goto_record_type(au, type);
-
-				uid = auparse_find_field(au, "uid");
-				if (uid && i == -1) {
-					i = auparse_get_field_int(au);
-					if (i != -1)
-						getpwuid_r(i, &pwd, buf, bufsize, &result);
-				}
-				goto_record_type(au, type);
-
-				tty = auparse_find_field(au, "tty");
-				if (tty)
-					tty = auparse_interpret_field(au);
-				goto_record_type(au, type);
-
-				gid = auparse_find_field(au, "gid");
-				goto_record_type(au, type);
-				euid = auparse_find_field(au, "euid");
-				goto_record_type(au, type);
-				suid = auparse_find_field(au, "suid");
-				goto_record_type(au, type);
-				fsuid = auparse_find_field(au, "fsuid");
-				goto_record_type(au, type);
-				egid = auparse_find_field(au, "egid");
-				goto_record_type(au, type);
-				sgid = auparse_find_field(au, "sgid");
-				goto_record_type(au, type);
-				fsgid = auparse_find_field(au, "fsgid");
-				goto_record_type(au, type);
-				ses = auparse_find_field(au, "ses");
-				goto_record_type(au, type);
-
-				if (extralen == 0) {
-					extralen += snprintf(extra+extralen, 1024, "gid\\=%s euid\\=%s suid\\=%s fsuid\\=%s egid\\=%s sgid\\=%s fsgid\\=%s ses\\=%s",
-						gid, euid, suid, fsuid, egid, sgid, fsgid, ses);
+					havecef = i;
+					cef_msg.msgname = "EXECVE";
+					cef_msg.msgdesc = "Unix Exec";
 				} else {
-					extralen += snprintf(extra+extralen, 1024, " gid\\=%s euid\\=%s suid\\=%s fsuid\\=%s egid\\=%s sgid\\=%s fsgid\\=%s ses\\=%s",
-						gid, euid, suid, fsuid, egid, sgid, fsgid, ses);
+					syslog(LOG_INFO, "Unhandled system call %u %s", i, sys);
 				}
 
-				exe = auparse_find_field(au, "exe");
-				if (exe)
-					exe = auparse_interpret_field(au);
+				cef_msg.attr = cef_add_attr(cef_msg.attr, "cs3Label=AuditKey cs3=", auparse_find_field(au, "key"));
+				goto_record_type(au, type);
+
+				if (auparse_find_field(au, "ppid"))
+					cef_msg.attr = cef_add_attr(cef_msg.attr, "cs5Label=ParentProcess cs5=", get_proc_name(auparse_get_field_int(au)));
+				goto_record_type(au, type);
+
+				i = 0;
+				if (auparse_find_field(au, "auid")) {
+					cef_msg.attr = cef_add_attr(cef_msg.attr, "suser=", get_username(auparse_get_field_int(au)));
+					cef_msg.attr = cef_add_attr(cef_msg.attr, "cn1Label=auid cn1=",  auparse_get_field_str(au));
+					i = 1;
+				}
+				goto_record_type(au, type);
+
+				if (auparse_find_field(au, "uid")) {
+					//if we have had no auid, we set suser to the real source user (else its set to the audited source user)
+					if (i == 0)
+						cef_msg.attr = cef_add_attr(cef_msg.attr, "suser=", get_username(auparse_get_field_int(au)));
+					cef_msg.attr = cef_add_attr(cef_msg.attr, "suid=", auparse_get_field_str(au));
+				}
+				goto_record_type(au, type);
+
+				cef_msg.attr = cef_add_attr(cef_msg.attr, "cs4Label=TTY cs4=", auparse_find_field(au, "tty"));
+				goto_record_type(au, type);
+				cef_msg.attr = cef_add_attr(cef_msg.attr, "dproc=", auparse_find_field(au, "exe"));
+				goto_record_type(au, type);
+
+				snprintf(extra+strlen(extra), 16, "pid\\=%s ", auparse_find_field(au, "pid"));
+				goto_record_type(au, type);
+				snprintf(extra+strlen(extra), 16, "gid\\=%s ", auparse_find_field(au, "gid"));
+				goto_record_type(au, type);
+				snprintf(extra+strlen(extra), 16, "euid\\=%s ", auparse_find_field(au, "euid"));
+				goto_record_type(au, type);
+				snprintf(extra+strlen(extra), 16, "suid\\=%s ", auparse_find_field(au, "suid"));
+				goto_record_type(au, type);
+				snprintf(extra+strlen(extra), 16, "fsuid\\=%s ", auparse_find_field(au, "fsuid"));
+				goto_record_type(au, type);
+				snprintf(extra+strlen(extra), 16, "egid\\=%s ", auparse_find_field(au, "egid"));
+				goto_record_type(au, type);
+				snprintf(extra+strlen(extra), 16, "sgid\\=%s ", auparse_find_field(au, "sgid"));
+				goto_record_type(au, type);
+				snprintf(extra+strlen(extra), 16, "fsgid\\=%s ", auparse_find_field(au, "fsgid"));
+				goto_record_type(au, type);
+				snprintf(extra+strlen(extra), 16, "ses\\=%s ", auparse_find_field(au, "ses"));
+				goto_record_type(au, type);
 				break;
 			default:
 				break;
@@ -383,24 +483,11 @@ static void handle_event(auparse_state_t *au,
 	if (!havecef)
 		return;
 
-	if (strlen(extra) >= 1024) {
-		extra[1024] = '\0';
-		strncpy(extrat, "Yes\0", 4);
+	if (strlen(extra) > 512) {
+		extra[512] = '\0';
+		cef_msg.attr = cef_add_attr(cef_msg.attr, "cs6Label=MsgTruncated cs6=", "Yes");
 	}
-
-	syslog(LOG_INFO, "CEF:0|Unix|auditd|1|%s|%s|3|end=%ld fname=%s dhost=%s suser=%s \
-suid=%s dproc=%s msg=%s \
-cn1Label=auid cn1=%s \
-cs1Label=Command cs1=%s \
-cs2Label=Truncated cs2=%s \
-cs3Label=AuditKey cs3=%s \
-cs4Label=TTY cs4=%s \
-cs5Label=ParentProcess cs5=%s \
-cs6Label=MsgTruncated cs6=%s\n",
-		msgname, msgdesc, au_time,
-		fname, hostname, pwd.pw_name ? pwd.pw_name: NULL,
-		uid, exe, extra, auid,
-		fullcmd, fullcmdt,
-		key, tty,
-		sppid, extrat);
+	cef_msg.attr = cef_add_attr(cef_msg.attr, "msg=", extra);
+	cef_msg.attr = cef_add_attr(cef_msg.attr, "dhost=", hostname);
+	syslog_cef_msg(cef_msg);
 }
